beab64a9656a6a8c80853c148ad68a20d31161b8
Shubhankar2803 <singhshubhankar561@gmail.com>
2025-03-30 11:40:28 +0530

    setting up folder structure and routes

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import Authform from '@/components/Authform'
+import React from 'react'
+
+const Signin = () => {
+  return <Authform type="sign-in" />
+}
+
+export default Signin
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import Authform from '@/components/Authform'
+import React from 'react'
+
+const Signup = () => {
+  return <Authform type="sign-up" />
+  
+}
+
+export default Signup
