8ee4e4634c3c15110a3339e454db3721386a6718
Shubhankar2803 <singhshubhankar561@gmail.com>
2025-04-06 00:00:38 +0530

    userid error solved

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
 import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
-import { dummyInterviews } from '@/constants'
+import { getCurrentUser, getInterview, getLatestInterviews } from '@/lib/actions/auth.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const user=await getCurrentUser();
+
+  const [userInterviews,latestInterviews] = await Promise.all([
+    await getInterview(user?.id!),
+    await getLatestInterviews({userId:user?.id!})
+    
+  ])
+  
+
+  const hasPastInterviews=userInterviews?.length>0;
+  const hasUpcomingInterviews=latestInterviews?.length>0;
   return (
 <>
 <section className='card-cta'>
@@ -28,9 +39,15 @@ const Page = () => {
 <section className='flex flex-col gap-6 mt-8'>
   <h2>Your Interviews</h2>
    <div className='interviews-section'>
-   {dummyInterviews.map((interview)=>(
-    <InterviewCard  { ... interview} key={interview.id} />
-   ))}
+   {
+    hasPastInterviews ?(
+      userInterviews?.map((interview)=>(
+            <InterviewCard  { ... interview} key={interview.id} />
+
+      ))
+    ):(<p>You haven't taken an interview yet</p>)
+
+   }
     </div>
 </section>
 
@@ -38,11 +55,17 @@ const Page = () => {
   <h2>Take an interview</h2>
 
   <div className='interviews-section'>
-  {dummyInterviews.map((interview)=>(
-    <InterviewCard  { ... interview}  key={interview.id} />
-   ))} 
+  {
+    hasUpcomingInterviews ?(
+      latestInterviews?.map((interview)=>(
+            <InterviewCard  { ... interview} key={interview.id} />
+
+      ))
+    ):(<p>There are no  new interviews available. </p>)
+
+   }
    
-  { /* <p>you haven't taken an interview yet</p> */}
+  { /*  */}
    
     </div>
 </section>
--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -2,13 +2,20 @@ import {generateText} from 'ai'
 import {google} from '@ai-sdk/google'
 import { getRandomInterviewCover } from '@/lib/utils';
 import { db } from '@/firebase/admin';
+import { getCurrentUser } from '@/lib/actions/auth.actions';
+import { Timestamp } from 'firebase/firestore';
 
 export async function GET(){
     return Response.json({success:true,data:'THANK YOU'},{status:200});
 }
 
 export async function POST(request:Request){
+    const user = await getCurrentUser();
+
  const {type,role,level,techstack,amount,userid}= await request.json();
+
+
+
  try {
     const {text: questions}=await generateText({
         model: google('gemini-2.0-flash-001'),
@@ -21,11 +28,12 @@ export async function POST(request:Request){
         Please return only the questions, without any additional text.
         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
         Return the questions formatted like this:
-        ["Question 1", "Question 2", "Question 3"]
+        ["Question 1", "Question 2", "Question 3"]  
         
         Thank you! <3
     `,
     });
+    console.log("Creating interview for user:", user);
 
     const interview = {
         role: role,
@@ -33,13 +41,14 @@ export async function POST(request:Request){
         level: level,
         techstack: techstack.split(","),
         questions: JSON.parse(questions),
-        userId: userid,
+        userId: user?.id,
         finalized: true,
         coverImage: getRandomInterviewCover(),
-        createdAt: new Date().toISOString(),
+        createdAt: Timestamp.now(),
       };
     await db.collection("interviews").add(interview)
-    return Response.json({success:true,data:interview},{status:200})
+
+    return Response.json({success:true},{status:200})
     
  } catch (error) {
     console.error(`here ${error}`)
